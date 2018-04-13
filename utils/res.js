8334a31fbf0dbd183373198fe744fe5b40f13885
ljf10000 <l_j_f@163.com>
2018-04-13 11:19:08 +0800

    .

--- utils/res.js
+++ utils/res.js
@@ -1,19 +1,54 @@
 // util.js
 
-function getRes(app, id) {
+function Get(app, id) {
 	let obj = $res[id];
 
 	return obj ? obj[app.lang] : "";
 }
 
+function join(app, ...keys) {
+	let count = keys.count;
+	if (0==count) {
+		return ""
+	}
+
+	let name = Get(app, keys[0]);
+	if (1==count) {
+		return name;
+	}
+
+	let split = (0==app.lang)?"":" ";
+	for (let i=1; i<count; i++) {
+		name += split + Get(app, keys[i]);
+	}
+
+	return name
+}
+
 const $res = {
-	wxLogin: ["微信登陆", "webcat login"],
-	mpLogin: ["小程序登陆", "mini program login"],
+	app: ["xxx", "xxx"],
+	success: ["成功", "success"],
+	fail: ["失败", "fail"],
+	login: ["登陆", "login"],
+
+	wx: ["微信", "webcat"],
+	mp: ["小程序", "mini program"],
 };
 
 const res = {
-	wxLogin: (app) => getRes(app, "wxLogin"),
-	mpLogin: (app) => getRes(app, "mpLogin"),
+	app: (app) => Get(app, "app"),
+	
+	success: (app) => Get(app, "success"),
+	fail: (app) => Get(app, "fail"),
+	login: (app) => Get(app, "login"),
+	wx: (app) => Get(app, "wx"),
+	mp: (app) => Get(app, "mp"),
+
+	wxLogin: (app) => join(app, "wx", "login"),
+	mpLogin: (app) => join(app, "mp", "login"),
+
+	wxLoginFail: (app) => join(app, "wx", "login", "fail"),
+	mpLoginFail: (app) => join(app, "mp", "login", "fail"),
 };
 
 module.exports = {
